218e862687881877537a35721ff0b09573b9a7d9
Sank2024 <sk11321@nyu.edu>
2025-07-23 13:17:05 -0400

    Added change

--- cpu.c
+++ cpu.c
@@ -9,16 +9,14 @@ struct PCB null_pcb() {
 struct PCB handle_process_arrival_pp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp) {
     if (current_process.process_id == 0) {
         new_process.execution_starttime = timestamp;
-        new_process.execution_endtime = timestamp + new_process.total_bursttime;
-        new_process.remaining_bursttime = new_process.total_bursttime;
+        new_process.execution_endtime = timestamp + new_process.remaining_bursttime;
         return new_process;
     }
     if (new_process.process_priority > current_process.process_priority) {
         ready_queue[*queue_cnt] = current_process;
         (*queue_cnt)++;
         new_process.execution_starttime = timestamp;
-        new_process.execution_endtime = timestamp + new_process.total_bursttime;
-        new_process.remaining_bursttime = new_process.total_bursttime;
+        new_process.execution_endtime = timestamp + new_process.remaining_bursttime;
         return new_process;
     } else {
         ready_queue[*queue_cnt] = new_process;
@@ -55,16 +53,14 @@ struct PCB handle_process_completion_pp(struct PCB ready_queue[QUEUEMAX], int *q
 struct PCB handle_process_arrival_srtp(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp) {
     if (current_process.process_id == 0) {
         new_process.execution_starttime = timestamp;
-        new_process.execution_endtime = timestamp + new_process.total_bursttime;
-        new_process.remaining_bursttime = new_process.total_bursttime;
+        new_process.execution_endtime = timestamp + new_process.remaining_bursttime;
         return new_process;
     }
-    if (new_process.total_bursttime < current_process.remaining_bursttime) {
+    if (new_process.remaining_bursttime < current_process.remaining_bursttime) {
         ready_queue[*queue_cnt] = current_process;
         (*queue_cnt)++;
         new_process.execution_starttime = timestamp;
-        new_process.execution_endtime = timestamp + new_process.total_bursttime;
-        new_process.remaining_bursttime = new_process.total_bursttime;
+        new_process.execution_endtime = timestamp + new_process.remaining_bursttime;
         return new_process;
     } else {
         ready_queue[*queue_cnt] = new_process;
@@ -79,10 +75,13 @@ struct PCB handle_process_completion_srtp(struct PCB ready_queue[QUEUEMAX], int 
     }
     int min_remaining_index = 0;
     int min_remaining_time = ready_queue[0].remaining_bursttime;
+    int min_arrival_time = ready_queue[0].arrival_timestamp;
     for (int i = 1; i < *queue_cnt; i++) {
-        if (ready_queue[i].remaining_bursttime < min_remaining_time) {
+        if (ready_queue[i].remaining_bursttime < min_remaining_time ||
+            (ready_queue[i].remaining_bursttime == min_remaining_time && ready_queue[i].arrival_timestamp < min_arrival_time)) {
             min_remaining_time = ready_queue[i].remaining_bursttime;
             min_remaining_index = i;
+            min_arrival_time = ready_queue[i].arrival_timestamp;
         }
     }
     struct PCB next_process = ready_queue[min_remaining_index];
@@ -98,11 +97,15 @@ struct PCB handle_process_completion_srtp(struct PCB ready_queue[QUEUEMAX], int 
 struct PCB handle_process_arrival_rr(struct PCB ready_queue[QUEUEMAX], int *queue_cnt, struct PCB current_process, struct PCB new_process, int timestamp, int time_quantum) {
     if (current_process.process_id == 0) {
         new_process.execution_starttime = timestamp;
-        new_process.execution_endtime = timestamp + MIN(new_process.total_bursttime, time_quantum);
-        new_process.remaining_bursttime = new_process.total_bursttime;
+        new_process.execution_endtime = timestamp + MIN(new_process.remaining_bursttime, time_quantum);
         return new_process;
     }
-    ready_queue[*queue_cnt] = new_process;
+    // Insert new process in FIFO order by arrival_timestamp
+    int i;
+    for (i = *queue_cnt; i > 0 && ready_queue[i-1].arrival_timestamp > new_process.arrival_timestamp; i--) {
+        ready_queue[i] = ready_queue[i-1];
+    }
+    ready_queue[i] = new_process;
     (*queue_cnt)++;
     return current_process;
 }
